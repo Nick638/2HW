be81ac08df75514032e702739913cae5e1c890f7
康恆 徐 <wa0125678665@>
2021-03-30 13:22:21 +0800

    不確定

--- 2HW/Default.aspx.cs
+++ 2HW/Default.aspx.cs
@@ -7,8 +7,18 @@ using System.Web.UI.WebControls;
 
 namespace _2HW {
     public partial class Default : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int N = 15, sum = 0;
+            for (int start = 1; start <= N; start++)
+            {
+                if (start % 2 == 0 || start % 3 == 0)
+                {
+                    sum += start; //sum = sum +start
 
+                }
+            }
+            Response.Write(sum);
         }
     }
 }
